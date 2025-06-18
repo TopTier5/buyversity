1b4a686165a8c69d3280978a67b76cd3e23a6f5d
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-06-18 23:54:04 +0000

    I started working on the cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,17 @@
+import CartNavbar from "../components/CartNavbar";
+import { TrashIcon } from "lucide-react";
+
 export default function Cart() {
     return (
-
-        <h1>welcome to cart page</h1>
+        <>
+        <CartNavbar />
+        <section className="ml-20 mr-20 mt-10 mb-10">
+            <div className="flex justify-between">
+        <h1 className="font-bold text-3xl text-transparent bg-clip-text bg-gradient-to-r from-purple-600  to-blue-600">Your Shopping Cart</h1>
+        <button className="text-white bg-[#DC2626] border rounded-md py-2 px-4 items-center flex justify-around gap-2">
+            <TrashIcon size={18} />Clear Cart</button>
+        </div>
+        </section>
+        </>
     );
 }
