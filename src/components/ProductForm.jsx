c0a3b5ff74b211218a03400b0a44ab7fa90cd020
James Junior Baah <jjbaah068@gmail.com>
2025-06-18 11:02:45 +0000

    initial updates to components

---
+++ src/components/ProductForm.jsx
@@ -0,0 +1,7 @@
+export default function ProductForm() {
+    return (
+        <section>
+            
+        </section>
+    );
+}
---
+++ src/components/ViewProducts.jsx
@@ -0,0 +1,9 @@
+
+
+export default function ViewProducts() {
+    return (
+        <section>
+
+        </section>
+    );
+}
