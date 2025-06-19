e98ba5a9c572091333f5a60d3c2121645cca1471
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-06-19 23:33:25 +0000

    Almost done witj the vendor-form page

--- src/components/CartComponent.jsx
+++ src/components/CartComponent.jsx
@@ -1,21 +1,35 @@
+import { TrashIcon } from "lucide-react";
 import vintage from "../assets/images/vintage.jpg"
 
-export default function CartComponent () {
+export default function CartComponent() {
     return (
         <>
-        <div className="flex flex-row">
-            <div className="border border-gray-300 rounded-md w-[60%] ml-12 md:ml-6 sm:ml-1 lg:ml-12
+            <div className="flex flex-row">
+                <div className="border border-gray-300 rounded-md w-[60%] ml-12 md:ml-6 sm:ml-1 lg:ml-12
              py-5 hover:shadow-2xl">
-                <div className="flex flex-row">
-                <img src={vintage} alt="vintage camera" className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-md ml-4 sm:ml-8" />
-                <div className=" ml-5  mt-1 flex flex-col">
-                <h3 className="font-bold ">Vintage Camera</h3>
-                <p>By John Doe</p>
-                <p>₵3000</p>
+                    <div className="flex justify-between">
+                        <div className="flex flex-row">
+                            <img src={vintage} alt="vintage camera" className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 rounded-md ml-4 sm:ml-8" />
+                            <div className=" ml-5  mt-1 flex flex-col">
+                                <h3 className="font-bold ">Vintage Camera</h3>
+                                <p className="text-sm text-gray-600">by John Doe</p>
+                                <p className="font-bold text-purple-600">₵3000</p>
+                            </div>
+                        </div>
+
+                        <div className=" flex flex-row items-center">
+                            <div className="border border-gray-400 rounded-md w-8 h-8 mr-5 flex justify-center items-center">
+                                 <div className="w-3 h-[2px] bg-gray-600"></div>  
+                            </div>
+                             <h1 className="mr-5">1</h1>
+                             <div className="border border-gray-400 rounded-md w-8 h-8 mr-5 flex justify-center items-center">
+                                 <div className="w-7 text-3xl text-center mb-3 text-gray-700 h-7">+</div>  
+                            </div>
+                            <div><TrashIcon size={18} /></div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
 
         </>
     );
--- src/pages/VendorForm.jsx
+++ src/pages/VendorForm.jsx
@@ -1,5 +1,92 @@
+import { useState } from "react";
+
+   
 export default function VendorForm() {
+
+    
     return (
-        <h1 className="text-center">welcome to vendorform</h1>
+
+        <section className="flex flex-col items-center min-h-screen bg-purple-200" >
+
+            <div className="mt-6 rounded-md min-h-screen w-full max-w-4xl bg-white px-6 py-8">
+                <div>
+                    <h1 className="w-full text-center text-2xl sm:text-3xl lg:text-3xl font-bold text-white bg-gradient-to-r from-purple-600 to-blue-600 py-6 rounded-t-md">
+                        Post Your Ad
+                    </h1>
+                </div>
+
+
+                <div>
+                    <form action="" className="flex flex-col  p-8">
+                        <div>
+                            <label htmlFor="Product Title">Product Title<span className="text-red-700">*</span></label>
+                            <input type="text" name="title" id="text" placeholder="Enter your product title"
+                                className="border border-gray-400 rounded-md w-[95%] h-9 mt-2" required />
+                        </div>
+                        <div className="mt-4 flex flex-row">
+                            <div className="flex-1/2">
+                                <label htmlFor="Category">Category<span className="text-red-700">*</span></label>
+                                <select name="category" id="category" className=" border border-gray-400 rounded-md  w-[90%]
+                          h-9 mt-2 " required>
+                                    <option selected disabled>Select a Category
+                                    </option>
+                                    <option value="electronics">Electronics</option>
+                                    <option value="furniture">Furniture</option>
+                                    <option value="clothing">Clothing</option>
+                                    <option value="books">Books</option>
+                                    <option value="sports">Sports</option>
+                                    <option value="other">Other</option>
+                                </select>
+                            </div>
+                            <div className="flex-1/2 ml-5">
+                                <label htmlFor="Price">₵Price<span className="text-red-700">*</span></label>
+                                <input type="number" name="price" id="price" className="border border-gray-400 rounded-md w-[90%] h-9
+                            mt-2 " required />
+                            </div>
+                        </div >
+                        <div className="mt-4">
+                            <label htmlFor="Description">Description<span className="text-red-700">*</span></label>
+                            <textarea name="description" id="text" placeholder="Describe your product in detail..."
+                                className="border border-gray-400 rounded-md w-[95%] h-20 mt-2" required />
+                        </div>
+
+                        <div className="mt-4">
+                            <label htmlFor="upload images">Upload Images<span className="text-red-700">*</span></label>
+                            <input type="file" name="image" id="image" accept="image/*" multiple className="border border-gray-400 rounded-md w-[95%] h-20 mt-2" required />
+                        </div>
+
+                        <div className="mt-4">
+                            <label className="">Condition <span className="text-red-700">*</span>
+                            </label>
+                            <div className="flex gap-3">
+                                <label className="inline-flex items-center"><input type="radio" name="condition" value="new" className="form-radio" required /> <span className="ml-2">New</span></label>
+                                <label className="inline-flex items-center"><input type="radio" name="condition" value="Used" className="form-radio" required /> <span className="ml-2">Used</span></label>
+                            </div>
+                        </div>
+                        <div className=" mt-4">
+                            <div><label htmlFor="University/Tertiary Institution" >University/Tertiary Institution</label>
+                                <select name="university/tertiaryinstituton" id="university/tertiayinstitution" className="border border-gray-400 rounded-md w-[95%] h-9 mt-2" required>
+                                <option selected disabled>Select a Category
+                                    </option>
+                                    <option value="university of ghana,legon">University of Ghana, Legon</option>
+                                    <option value="university of cape coast">University of Cape Coast</option>
+                                    <option value="ashesi university">Ashesi University</option>
+                                    <option value="central university">Central University</option>
+                                    <option value="kwame nkrumah university of science and technology">Kwame Nkrumah University of Science and Technology</option>
+                                    <option value="university of health and allied sciences">University of Health and Allied Sciences</option>
+                                    <option value="university of professional studies">University of Professional Studies</option>
+                                    <option value="ghana technology communication university">Ghana Technology Communication University</option>
+                                    <option value="regional maritime university">Regional Maritime University</option>
+                                    Accra Technical University
+                                    <option value="other">Other</option>
+                                </select>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+
+        </section>
     );
 }
