f55f3ff80909878c499a753cdf69dbc50345a7cc
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-06-20 16:11:54 +0000

    added image for register

--- src/pages/VendorDashboard.jsx
+++ src/pages/VendorDashboard.jsx
@@ -1,21 +1,32 @@
 import { ArrowLeft } from "lucide-react"
+import { User } from "lucide-react"
 
 export default function VendorDashboard() {
 
     return(
 
          <nav className="flex justify-between py-3 px-6 bg-white shadow-xl">
-            <ArrowLeft className="w-6 h-6" />
-            <div className="flex items-center gap-2 ml-15">
-                <h1 className="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-purple-600  to-blue-600">Back to Marketplace</h1>
+            <div className="flex items-center gap-2 ml-4 sm:ml-6 md:ml-10  ">
+                 <ArrowLeft className="w-5 h-5 sm:w-6 sm:h-6 text-white bg-gradient-to-r
+                  from-purple-600 to blue-600 rounded cursor-pointer" />
+                <h1 className="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-purple-600  to-blue-600 cursor-pointer">Back to Marketplace</h1>
                     
             </div>
-            <div className="mr-10">
-                <button className="border border-[#D8B4FE] text-[#7E22CE] pt-2 pb-2 pl-3 pr-3
-                text-sm font-semibold rounded-md  ">Continue Shopping</button>
+            <div className="mr-10 flex flex-row">
+                <div className="w-10 h-10 rounded-full bg-gradient-to-r from-purple-600 to-blue-600 
+                flex items-center justify-center ">
+                    <User className="w-5 h-5 text-white" />
+                </div>
+                <div><button className="border border-[#D8B4FE] bg-gradient-to-r from-purple-600 to-blue-600
+                 text-white pt-2 pb-2 pl-3 pr-3
+                text-sm font-semibold rounded-md cursor-pointer ">+ Post New Ad</button>
+                </div>
             </div>
-           
+
+
         </nav>
+
+        
     )
 
 }
