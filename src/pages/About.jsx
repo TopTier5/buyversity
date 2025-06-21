7e9cb5cd2481a47fe0f497916a2e2fbf5134535d
James Junior Baah <jjbaah068@gmail.com>
2025-06-21 23:45:58 +0000

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import about3 from "../assets/images/about3.png"
+import { Target, Handshake, Users, BadgeCheck, Cable } from "lucide-react";
 
 
 
@@ -86,6 +87,96 @@ export default function About() {
 </div>
             </div>
 
+            <div className="bg-[#f9fafb] py-20 px-4 mt-20">
+                <div className="text-center">
+                <h3 className="text-4xl font-bold">Why Students Choose Us</h3>
+                 <div className="w-25 h-1 bg-gradient-to-r from-purple-600 to-blue-600 mx-auto rounded-full mt-5"></div>
+                 <p className="text-gray-600 mt-4 text-lg max-w-2xl mx-auto">
+          We're more than a marketplace - we're a community built to support student success.
+        </p>
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-5xl mx-auto">
+                    <div className="flex items-start bg-white rounded-xl py-12 px-4 shadow-md mt-15">
+                        <div className="bg-gradient-to-r from-purple-500 to-blue-500 text-white p-3 rounded-md">
+                            <Target />
+                        </div>
+                        <div >
+                            <h4 className="text-lg font-bold mb-3 ml-4">Student First Focus</h4>
+                            <p className="text-gray-600 text-md ml-4">Built specifically for students, by students. We understand your budget constraints and unique needs.</p>
+                        </div>
+                    </div>
+
+                    <div className="flex items-start bg-white rounded-xl py-12 px-4 shadow-md mt-15">
+          <div className="bg-gradient-to-r from-purple-500 to-blue-500 text-white p-3 rounded-md">
+            <Handshake />
+          </div>
+          <div>
+            <h4 className="text-lg font-bold mb-3 ml-4">Safe Transactions</h4>
+            <p className="text-gray-600 text-md ml-4">
+               Every listing on BuyVersity is student-to-student, giving you peace of mind through familiar, local connections. 
+               Our platform is built for trust with real students, verified profiles, and a shared campus network.
+            </p>
+          </div>
+        </div>
+
+         <div className="flex items-start bg-white rounded-xl py-12 px-4 shadow-md mt-4">
+          <div className="bg-gradient-to-r from-purple-500 to-blue-500 text-white p-3 rounded-md">
+            <Users />
+          </div>
+          <div>
+            <h4 className="text-lg font-bold mb-3 ml-4">Campus Community</h4>
+            <p className="text-gray-600 text-md ml-4">
+                Connect with fellow students on your campus and nearby universities for local pickup and networking.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex items-start bg-white rounded-xl py-12 px-4 shadow-md mt-4">
+          <div className="bg-gradient-to-r from-purple-500 to-blue-500 text-white p-3 rounded-md">
+            <BadgeCheck />
+          </div>
+          <div>
+            <h4 className="text-lg font-bold mb-3 ml-4">Quality Guaranteed</h4>
+            <p className="text-gray-600 text-md ml-4">
+               Every item is verified and rated by our community to ensure you get exactly what you expect.
+            </p>
+          </div>
+        </div>
+                </div>
+            </div>
+
+
+        <div className="bg-white mt-20">
+            <div className="text-center">
+                <h3 className="text-4xl font-bold">What Students Buy & Sell</h3>
+                 <div className="w-25 h-1 bg-gradient-to-r from-purple-600 to-blue-600 mx-auto rounded-full mt-5"></div>
+            </div>
+
+            <div className="grid grid-cols-1 mx-auto">
+            <div className="flex items-start mt-15">
+                <div className="bg-gradient-to-r from-purple-500 to-blue-500 text-white p-3 rounded-md">
+                    <Cable />
+                </div>
+            </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
 
             
 
