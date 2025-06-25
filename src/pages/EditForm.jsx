3ba5c0f4c200f6b3d29d87ba90ed642f5d46b126
vonette-ab <vonwrkspc@gmail.com>
2025-06-25 15:19:42 +0000

    added

--- src/pages/EditForm.jsx
+++ src/pages/EditForm.jsx
@@ -1,6 +1,11 @@
-import { useState } from "react";
+import { use, useState } from "react";
 import VendorNav from "../components/VendorNav";
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
+import { useEffect } from "react";
+import { apiClient } from "../api/client";
+import { ArrowLeft} from "lucide-react"
+import { User } from "lucide-react"
+import { Link } from "react-router";
 
 
 export default function EditForm() {
@@ -10,13 +15,72 @@ export default function EditForm() {
 
      console.log("Editing ad with ID:", id)
 
+
     
+
+    const [advert, setAdvert] = useState({})
+    const getAdvert = () => {
+        apiClient.get(`/api/v1/adverts/${id}`)
+            .then((response) => {
+                console.log(response.data);
+                setAdvert(response.data.data);
+            })
+        .catch ((error) => {
+            console.log(error);
+        })
+
+    }
+
+        useEffect(getAdvert, []);
+
+
+        const putAdvert = async (data) => {
+            try {
+            
+            const response = await apiClient.put(`/api/v1/adverts/${id}`, data, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            console.log (response);
+            navigate(-1); 
+        } catch (error) {
+            console.log(error);
+        };
+
+    }
+
+        
+   
 
     return (
 
         <>
-            <VendorNav />
 
+         <nav className="flex justify-between py-3 px-6 bg-white shadow-xl">
+                <Link to="/">
+                <div className="flex items-center gap-2 ml-4 sm:ml-6 md:ml-10  ">
+                    <ArrowLeft className="w-5 h-5 sm:w-6 sm:h-6 text-white bg-gradient-to-r
+                  from-purple-600 to blue-600 rounded cursor-pointer" />
+                    <h1 className="font-bold text-xs text-transparent bg-clip-text bg-gradient-to-r from-purple-600  to-blue-600 cursor-pointer">Back to Marketplace</h1>
+                </div></Link>
+               
+                <div className="flex flex-row sm:flex-row mr-4 sm:mr-6 md:mr-10 lg:mr-16 ">
+                    <div className="mr-3 w-10 h-10  rounded-full bg-gradient-to-r from-purple-600 to-blue-600 flex items-center justify-center ">
+                        <User className="w-5 h-5 text-white " />
+                    </div>
+                    <Link to="/vendor-dashboard">
+                        <div className="border border-[#D8B4FE] bg-gradient-to-r from-purple-600 to-blue-600
+                 text-white pt-2 pb-2 pl-3 pr-3 hover:shadow-2xl shadow-blue-700
+                text-sm font-semibold rounded-md cursor-pointer ">Back to Dashboard
+                        </div>
+
+
+                    </Link>
+
+                </div>
+            </nav>
+        
 
             <section className="flex flex-col items-center min-h-screen bg-purple-200" >
 
@@ -29,7 +93,7 @@ export default function EditForm() {
 
 
                     <div>
-                        <form action="" className="flex flex-col  p-8">
+                        <form action={putAdvert} className="flex flex-col  p-8">
                             <div>
                                 <label htmlFor="Product Title">Product Title<span className="text-red-700">*</span></label>
                                 <input type="text" name="title" id="text" placeholder="Enter your product title"
@@ -39,7 +103,8 @@ export default function EditForm() {
                                 <div className="flex-1/2">
                                     <label htmlFor="Category">Category<span className="text-red-700">*</span></label>
                                     <select name="category" id="category" className=" border border-gray-400 rounded-md  w-[90%]
-                                  h-9 mt-2 " required>
+                                  h-9 mt-2 " required
+                                  defaultValue= {advert.category}> 
                                         <option selected disabled>Select a Category
                                         </option>
                                         <option value="electronics">Electronics</option>
@@ -52,13 +117,15 @@ export default function EditForm() {
                                 <div className="flex-1/2 ml-5">
                                     <label htmlFor="Price">Price (₵)<span className="text-red-700">*</span></label>
                                     <input type="number" name="price" id="price" placeholder="0.00" className="border border-gray-400 rounded-md w-[90%] h-9
-                                    mt-2 " required />
+                                    mt-2 " required
+                                    defaultValue= {advert.price} />
                                 </div>
                             </div >
                             <div className="mt-4">
                                 <label htmlFor="Description">Description<span className="text-red-700">*</span></label>
                                 <textarea name="description" id="text" placeholder="Describe your product in detail..."
-                                    className="border border-gray-400 rounded-md w-[95%] h-20 mt-2" required />
+                                    className="border border-gray-400 rounded-md w-[95%] h-20 mt-2" required 
+                                    defaultValue= {advert.description}/>
                             </div>
 
 
@@ -82,28 +149,17 @@ export default function EditForm() {
                                         <option value="other">Other</option>
                                     </select>
                                 </div>
-                                <div className="mt-4">
-                                    <label htmlFor="customUniversity">Enter Your University If not listed above <span className="text-red-700">*</span></label>
-                                    <input
-                                        type="text"
-                                        name="customUniversity"
-                                        id="customUniversity"
-                                        placeholder="Enter your university name"
-                                        value={customUniversity}
-                                        onChange={(e) => setCustomUniversity(e.target.value)}
-                                        className="border border-gray-400 rounded-md w-[95%] h-9 mt-2"
-
-                                    />
-                                </div>
+                                
                                 <div className="mt-4">
                                     <label htmlFor="Location"> Location<span className="text-red-700">*</span></label>
                                     <input type="text" id="location" placeholder="Enter your location" className="border border-gray-400 rounded-md w-[95%] h-9
-                                    mt-2 " required />
+                                    mt-2 " required 
+                                    defaultValue= {advert.location}/>
                                 </div>
 
                                 <div class=" flex w-full h-[80px] max-w-md items-center justify-center mt-4  mx-auto  gap-4 text-base font-medium">
-                                    <button class="flex-1 bg-white border border-gray-400 text-black text-base font-medium rounded-md p-4 hover:bg-gray-300 ">Cancel</button>
-                                    <button class="flex-1  from-purple-500 to-blue-500 text-white text-base font-medium rounded-md p-4 hover:bg-[#941BFA]">Update Ad</button>
+                                   
+                                    <button class="flex-1 bg-gradient-to-r from-purple-600 to-blue-600   font-medium rounded-md p-4 text-white">Update Ad</button>
                                 </div>
 
                             </div>
@@ -112,7 +168,7 @@ export default function EditForm() {
 
                 </div>
 
-            </section>---
+            </section>
         </>
     );
 }
